f07fe533427adf2d4374def4b5f0d12ec68d5fe0
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-02-24 12:13:38 +0100

    Navigation implemented and working

---
+++ SmartHome-Mobile/app/src/main/java/be/ucll/java/mobile/smarthome_mobile/ConsumptionActivity.java
@@ -0,0 +1,46 @@
+package be.ucll.java.mobile.smarthome_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ConsumptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumption);
+
+        //initialise navigation variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        //set selected
+        bottomNavigationView.setSelectedItemId(R.id.navConsumption);
+        //perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navConsumption:
+                        return true;
+
+                    case R.id.navManage:
+                        startActivity(new Intent(getApplicationContext(), ManageActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.navHouses:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+}
