a73f939e776860d685844dc73a9ecb646649ae1b
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-03-03 12:42:24 +0100

    Create User met response error

---
+++ SmartHome-Mobile/app/build/generated/source/buildConfig/debug/be/ucll/java/mobile/smarthome_mobile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package be.ucll.java.mobile.smarthome_mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "be.ucll.java.mobile.smarthome_mobile";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
