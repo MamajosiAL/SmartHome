00188f8abc69ffc6d3e9a067a7e20eb871370e6e
JBoon02 <r0748142@ucll.be>
2022-03-05 14:24:43 +0100

    Testing
    - Enkele testen falen door gebrek if's
    - Enkele testen runnen niet door falende test logica

---
+++ SmartHome/src/test/java/com/ucll/smarthome/controller/HouseControllerTest.java
@@ -0,0 +1,284 @@
+package com.ucll.smarthome.controller;
+
+import com.ucll.smarthome.AbstractIntegrationTest;
+import com.ucll.smarthome.dto.HouseDTO;
+import com.ucll.smarthome.dto.UserDTO;
+import com.ucll.smarthome.persistence.entities.House;
+import com.ucll.smarthome.persistence.repository.HouseDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "TestUser", userDetailsServiceBeanName = "UserDetailService")
+class HouseControllerTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private HouseController houseController;
+
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private HouseDAO houseDAO;
+
+    private UserDTO user1 = new UserDTO.Builder()
+            .firstname("Testing")
+            .name("User")
+            .username("TestUser")
+            .email("TestingUser@glock.com")
+            .password("TUROX")
+            .build();
+
+    private HouseDTO testHouse1 = new HouseDTO.Builder()
+            .name("testHouse")
+            .build();
+
+    @BeforeEach
+    void setUp() {
+        userController.createUser(user1);
+    }
+
+    private void addHouse(){
+        houseController.createHouse(testHouse1);
+    }
+
+    //Happy path create house
+    @Test
+    void createHouse() {
+        HouseDTO newHouse = new HouseDTO.Builder()
+                .name("newTestHouse")
+                .build();
+
+        houseController.createHouse(newHouse);
+
+        assertTrue(houseController.getHousesByUser().contains(newHouse));
+    }
+
+    @Test
+    void createHouseNameNull() {
+        addHouse();
+
+        String houseName = null;
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder().name(houseName)
+                .id(searchedHouse.getId())
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> houseController.createHouse(dto));
+    }
+
+    @Test
+    void createHouseNameSpace() {
+        addHouse();
+        String houseName = " ";
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder().name(houseName)
+                .id(searchedHouse.getId())
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> houseController.createHouse(dto));
+    }
+
+    @Test
+    void createHouseNameEmpty() {
+        addHouse();
+        String houseName = "";
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder().name(houseName)
+                .id(searchedHouse.getId())
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> houseController.createHouse(dto));
+    }
+
+    @Test
+    void createHouseuserIDEmtpy() {
+        addHouse();
+        String houseName = "MyCrib";
+
+        HouseDTO dto = new HouseDTO.Builder().name(houseName)
+                .id(0)
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> houseController.createHouse(dto));
+    }
+
+    @Test
+    void createHouseuserIDNegative() {
+        addHouse();
+        String houseName = "MyCrib";
+
+        HouseDTO dto = new HouseDTO.Builder().name(houseName)
+                .id(-99)
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> houseController.createHouse(dto));
+    }
+
+    //Happy path update house
+    @Test
+    void updateHouse() {
+        addHouse();
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+        System.out.println(searchedHouse.getName());
+
+        HouseDTO alteredHouse = new HouseDTO.Builder()
+                .id(searchedHouse.getId())
+                .name("newTestHouse")
+                .build();
+
+        houseController.updateHouse(alteredHouse);
+
+        assertEquals(alteredHouse.getName(), houseController.getHouseById(searchedHouse.getId()).getName());
+        assertNotEquals(alteredHouse.getName(), testHouse1.getName());
+    }
+
+    @Test
+    void updateHouseNameNull() {
+        addHouse();
+
+        String testHouseName = testHouse1.getName();
+        String alteredHouseName = null;
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder()
+                .id(searchedHouse.getId())
+                .name(alteredHouseName)
+                .build();
+
+        assertThrows(IllegalArgumentException.class,() -> houseController.updateHouse(dto));
+    }
+
+    @Test
+    void updateHouseNameEmpty() {
+        addHouse();
+
+        String testHouseName = testHouse1.getName();
+        String alteredHouseName = "";
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder()
+                .id(searchedHouse.getId())
+                .name(alteredHouseName)
+                .build();
+
+        assertThrows(IllegalArgumentException.class,() -> houseController.updateHouse(dto));
+    }
+
+    @Test
+    void updateHouseNameSpace() {
+        addHouse();
+
+        String testHouseName = testHouse1.getName();
+        String alteredHouseName = " ";
+
+        HouseDTO searchedHouse = houseController.getAllHouses().stream().filter(p -> p.getName().equals(testHouse1.getName())).findFirst().get();
+
+        HouseDTO dto = new HouseDTO.Builder()
+                .id(searchedHouse.getId())
+                .name(alteredHouseName)
+                .build();
+
+        assertThrows(IllegalArgumentException.class,() -> houseController.updateHouse(dto));
+    }
+
+    @Test
+    void updateHouseID0() {
+        addHouse();
+
+        String testHouseName = testHouse1.getName();
+        String alteredHouseName = "MyCrib";
+
+        HouseDTO dto = new HouseDTO.Builder()
+                .id(0)
+                .name(alteredHouseName)
+                .build();
+
+        assertThrows(IllegalArgumentException.class,() -> houseController.updateHouse(dto));
+    }
+
+    @Test
+    void updateHouseIDNegative() {
+        addHouse();
+
+        String testHouseName = testHouse1.getName();
+        String alteredHouseName = "MyCrib";
+
+        HouseDTO dto = new HouseDTO.Builder()
+                .id(-789)
+                .name(alteredHouseName)
+                .build();
+
+        assertThrows(IllegalArgumentException.class,() -> houseController.updateHouse(dto));
+    }
+
+
+
+    @Test
+    void getHouseById() {
+        addHouse();
+
+        House searechHouse = houseDAO.findAll().stream()
+                .filter(p-> p.getName().equals(testHouse1.getName()))
+                .findFirst().get();
+
+        HouseDTO dto = houseController.getHouseById(searechHouse.getHouseId());
+
+        assertEquals(dto.getName(), testHouse1.getName());
+        assertEquals(dto.getId(), testHouse1.getId());
+    }
+
+    @Test
+    void getHousesByUser() {
+        addHouse();
+
+        assertTrue(houseController.getHousesByUser().contains(testHouse1));
+    }
+
+    @Test
+    void getAllHouses() {
+        addHouse();
+        addHouse();
+
+        assertEquals(2, houseController.getAllHouses().size());
+    }
+
+    @Test
+    void deleteHouse() {
+        addHouse();
+
+        House searechHouse = houseDAO.findAll().stream()
+                .filter(p-> p.getName().equals(testHouse1.getName()))
+                .findFirst().get();
+
+        houseController.deleteHouse(searechHouse.getHouseId());
+
+        assertEquals(0, houseController.getAllHouses().size());
+    }
+
+    @Test
+    void deleteUserFromHouse() {
+        UserDTO testUser2 = new UserDTO.Builder()
+                .firstname("Testing2")
+                .name("User2")
+                .username("TestingUser2")
+                .email("TestingUser@glock.com2")
+                .password("TUROX2")
+                .build();
+
+        userController.createUser(testUser2);
+        //TODO: Once addUserToHouse possible
+    }
+}
---
+++ SmartHome/src/test/java/com/ucll/smarthome/controller/RoomControllerTest.java
@@ -0,0 +1,213 @@
+package com.ucll.smarthome.controller;
+
+import com.ucll.smarthome.AbstractIntegrationTest;
+import com.ucll.smarthome.dto.HouseDTO;
+import com.ucll.smarthome.dto.RoomDTO;
+import com.ucll.smarthome.dto.UserDTO;
+import com.ucll.smarthome.persistence.entities.House;
+import com.ucll.smarthome.persistence.entities.Room;
+import com.ucll.smarthome.persistence.repository.HouseDAO;
+import com.ucll.smarthome.persistence.repository.RoomDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "TestUser", userDetailsServiceBeanName = "UserDetailService")
+class RoomControllerTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private HouseController houseController;
+
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private RoomController roomController;
+
+    @Autowired
+    private HouseDAO houseDAO;
+
+    @Autowired
+    private RoomDAO roomDAO;
+
+    private UserDTO user1 = new UserDTO.Builder()
+            .firstname("Testing")
+            .name("User")
+            .username("TestUser")
+            .email("TestingUser@glock.com")
+            .password("TUROX")
+            .build();
+
+    private HouseDTO testHouse1 = new HouseDTO.Builder()
+            .name("testHouse")
+            .build();
+
+    private RoomDTO testRoom = new RoomDTO.Builder()
+            .name("testRoom")
+            .build();
+
+    private House searechHouse;
+
+    @BeforeEach
+    void setUp() {
+        userController.createUser(user1);
+    }
+
+    private void addBeforeTest(){
+        houseController.createHouse(testHouse1);
+
+        searechHouse = houseDAO.findAll().stream()
+                .filter(p-> p.getName().equals(testHouse1.getName()))
+                .findFirst().get();
+    }
+
+    //Happy path create Room
+    @Test
+    void createRoom() {
+        addBeforeTest();
+
+        houseController.getHousesByUser();
+
+        RoomDTO testRoom = new RoomDTO.Builder()
+                .name("testRoom")
+                .houseid(searechHouse.getHouseId())
+                .build();
+
+        roomController.createRoom(testRoom);
+
+        assertEquals(roomController.getRoomsByHouse(searechHouse.getHouseId()).size(), 1);
+    }
+
+    @Test
+    void createRoomNullName(){
+        RoomDTO roomNullName = testRoom;
+        roomNullName.setName(null);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.createRoom(roomNullName));
+    }
+
+    @Test
+    void createRoomEmptyName(){
+        RoomDTO roomEmptyName = testRoom;
+        roomEmptyName.setName("");
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.createRoom(roomEmptyName));
+    }
+
+    @Test
+    void createRoomHouseID0(){
+        RoomDTO roomID0 = testRoom;
+        roomID0.setHouseid(0);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.createRoom(roomID0));
+    }
+
+    @Test
+    void createRoomHouseIDNegative(){
+        RoomDTO roomIDNegative = testRoom;
+        roomIDNegative.setHouseid(-666);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.createRoom(roomIDNegative));
+    }
+
+    @Test
+    void createRoomSpaceAsName(){
+        RoomDTO roomNullSpace = testRoom;
+        roomNullSpace.setName(" ");
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.createRoom(roomNullSpace));
+    }
+
+    //Happy Path Update Room
+    @Test
+    void updateRoom() {
+        Room room = roomDAO.findAll().stream()
+                .filter(p -> p.getName().equals(testRoom.getName()))
+                .findFirst()
+                .get();
+
+        RoomDTO testRoomUpdate = new RoomDTO.Builder()
+                .id(room.getRoomID())
+                .name("RoomUpdated")
+                .build();
+
+        roomController.updateRoom(testRoomUpdate);
+
+        RoomDTO checkRoom = roomController.getRoomById(room.getRoomID());
+        assertEquals(checkRoom.getName(), testRoomUpdate.getName());
+        assertNotEquals(checkRoom.getName(), testRoom.getName());
+
+    }
+
+    @Test
+    void updateRoomNullName(){
+        RoomDTO roomNullName = testRoom;
+        roomNullName.setName(null);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.updateRoom(roomNullName));
+    }
+
+    @Test
+    void updateRoomEmptyName(){
+        RoomDTO roomEmptyName = testRoom;
+        roomEmptyName.setName("");
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.updateRoom(roomEmptyName));
+    }
+
+    @Test
+    void updateRoomHouseID0(){
+        RoomDTO roomID0 = testRoom;
+        roomID0.setHouseid(0);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.updateRoom(roomID0));
+    }
+
+    @Test
+    void updateRoomHouseIDNegative(){
+        RoomDTO roomIDNegative = testRoom;
+        roomIDNegative.setHouseid(-666);
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.updateRoom(roomIDNegative));
+    }
+
+    @Test
+    void updateRoomSpaceAsName(){
+        RoomDTO roomNullSpace = testRoom;
+        roomNullSpace.setName(" ");
+
+        assertThrows(IllegalArgumentException.class, () -> roomController.updateRoom(roomNullSpace));
+    }
+
+    @Test
+    void getRoomById() {
+        Room room = roomDAO.findAll().stream()
+                .filter(p -> p.getName().equals(testRoom.getName()))
+                .findFirst()
+                .get();
+
+        assertEquals(testRoom.getName(), roomController.getRoomById(room.getRoomID()).getName());
+        assertEquals(room.getRoomID(), roomController.getRoomById(room.getRoomID()).getId());
+    }
+
+    @Test
+    void getRoomsByHouse() {
+        assertTrue(roomController.getRoomsByHouse(searechHouse.getHouseId()).contains(testRoom));
+    }
+
+    @Test
+    void deleteRoom() {
+        Room room = roomDAO.findAll().stream()
+                .filter(p -> p.getName().equals(testRoom.getName()))
+                .findFirst()
+                .get();
+
+        roomController.deleteRoom(room.getRoomID());
+
+        assertEquals(0, roomController.getRoomsByHouse(searechHouse.getHouseId()).size());
+    }
+}
