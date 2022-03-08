3d391469f88cf1d881ad2c82130681656660616a
Allan Rasson <r0672070@ucll.be>
2022-03-08 16:14:00 +0100

    fix voor admin te registreren

--- SmartHome/src/main/java/com/ucll/smarthome/dto/House_UserDTO.java
+++ SmartHome/src/main/java/com/ucll/smarthome/dto/House_UserDTO.java
@@ -3,6 +3,8 @@ package com.ucll.smarthome.dto;
 public class House_UserDTO {
 
     private long id;
+    private long houseid;
+    private long userid;
     private boolean isadmin;
 
     public House_UserDTO() {
@@ -11,6 +13,8 @@ public class House_UserDTO {
     private House_UserDTO(Builder builder) {
         setId(builder.id);
         setIsadmin(builder.isadmin);
+        setUserid(builder.userid);
+        setHouseid(builder.houseid);
     }
 
 
@@ -22,18 +26,35 @@ public class House_UserDTO {
         this.id = id;
     }
 
-    public boolean getIsadmin() {
-        return isadmin;
-    }
-
     public void setIsadmin(boolean isadmin) {
         this.isadmin = isadmin;
     }
 
+    public long getHouseid() {
+        return houseid;
+    }
+
+    public void setHouseid(long houseid) {
+        this.houseid = houseid;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public boolean isIsadmin() {
+        return isadmin;
+    }
 
     public static final class Builder {
         private long id;
         private boolean isadmin;
+        private long userid;
+        private long houseid;
 
         public Builder() {
         }
@@ -45,6 +66,16 @@ public class House_UserDTO {
 
         public Builder isadmin(boolean val) {
             isadmin = val;
+            return this;
+        }
+
+        public Builder userid(long val){
+            userid = val;
+            return this;
+        }
+
+        public Builder houseid(long val){
+            houseid = val;
             return this;
         }
 
