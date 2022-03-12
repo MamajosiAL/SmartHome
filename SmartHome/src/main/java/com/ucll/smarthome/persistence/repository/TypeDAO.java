e432d558dc4c7835d9eb2220c522c96b9fc21c31
Allan Rasson <r0672070@ucll.be>
2022-03-12 21:41:36 +0100

    kleine aanpassingen voor android

--- SmartHome/src/main/java/com/ucll/smarthome/persistence/repository/TypeDAO.java
+++ SmartHome/src/main/java/com/ucll/smarthome/persistence/repository/TypeDAO.java
@@ -4,7 +4,10 @@ import com.ucll.smarthome.persistence.entities.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface TypeDAO extends JpaRepository<Type,Long> {
+    Optional<Type> findByName(String name);
 }
